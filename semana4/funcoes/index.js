49d3b8d8c81b69ca717d36eda0b09f92681cd9bd
Ana Zimerer <ana.zimerer@gmail.com>
2020-05-04 16:52:41 -0300

    Aula Funções - Resolvido até o item 4

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,34 @@
+//EXERCICIO 01
+//
+// A) Para minhaFuncao(2) será retornado indefinido
+// B) Para minhaFuncao(5) será retornado [0,1,0,1,2,3]
+// C) Para minhaFuncao(8) será retornado [0,1,0,1,2,3,0,1,2,3,4,5]
+
+//EXERCÍCIO 02
+//
+//A) console.log(funcao(arrayDeNomes, "Darvas")); =  retorna 0
+//   console.log(funcao(arrayDeNomes, "João")); = retorna 2
+//   console.log(funcao(arrayDeNomes, "Paula")); = retorna undefined. Caso tivesse escrito Paulinha, retornaria 3
+//
+//B)
+
+
+//EXERCICIO 03 
+//Para cada posição de uma array, são modificados os resultados A e B. No resultado A, é adicionado o valor da posição. 
+//Para o resultado B, é multiplicado o valor da posição analisada. Todos esses resultados são adicionados em uma nova array
+//e mostra essa nova array
+
+//EXERCÍCIO 04
+/*
+let calculoIdadeDeCachorro = (anosHumanos) => {
+    let idadeCachorro= anosHumanos * 7
+    return idadeCachorro;
+}
+anosHumanos= prompt("Digite a idade de cachorro")
+anosHumanos= Number (anosHumanos)
+
+console.log("Sua idade de cachorro é: " + calculoIdadeDeCachorro(anosHumanos));
+*/
+
+valor=1120/10
+alert(valor)
