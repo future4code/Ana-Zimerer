bebd16c47a4826867d5dd47eaeca3f82ef10cbd6
Ana Zimerer <ana.zimerer@gmail.com>
2020-05-12 15:07:08 -0300

    Alterações na pasta semana 5

---
+++ semana5/introducao-ao-react/aula-introducao/build/precache-manifest.be1ffc4220b5462ca6f4c78d0fec69f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b04540ca8f98ff8c2797f430ee09e6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7711bb872349438f0dc1",
+    "url": "/static/css/main.7073c9df.chunk.css"
+  },
+  {
+    "revision": "7871caabcca17228db33",
+    "url": "/static/js/2.dcb09286.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dcb09286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7711bb872349438f0dc1",
+    "url": "/static/js/main.55f9d6c4.chunk.js"
+  },
+  {
+    "revision": "321fc01358a2bcbb12b2",
+    "url": "/static/js/runtime-main.5dd23f1e.js"
+  }
+]);
