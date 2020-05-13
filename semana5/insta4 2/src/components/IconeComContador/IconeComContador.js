66e21c2145334f46888dad9b659f1e97b5d81cd2
Ana Zimerer <ana.zimerer@gmail.com>
2020-05-13 16:48:05 -0300

    Componentes de Classe- Exercicio principal

---
+++ semana5/insta4 2/src/components/IconeComContador/IconeComContador.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './IconeComContador.css'
+
+export function IconeComContador(props) {
+	//esse props vem de onde? do post.js, que manda do estado dele e das condições
+	//vai mostrar a imagem das curtidas e o num de curtidas
+	//como é chamado duas vezes, é sobre as curtidas e comentarios
+	return <div className={'icon-container'}>
+		<img alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
+		<p>{props.valorContador}</p>
+	</div>
+}
---
+++ semana5/insta4 2/src/components/NovoComentario/NovoComentario/NovoComentario.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './NovoComentario.css';
+
+
+export function NovoComentario (props){
+    return
+        <div className={'comment-container'}>
+            <label>{props.comentario}</label>
+        </div>    
+}
