5413aeda19e25838cc69de2d3215db15e8ceffc4
Ana Zimerer <ana.zimerer@gmail.com>
2020-07-03 15:33:42 -0300

    Funcionalidade: deletar tarefas e início de testes

--- semana11/planner-testes/src/App.js
+++ semana11/planner-testes/src/App.js
@@ -24,6 +24,10 @@ function App() {
 	}
 
 	useEffect(()=>{		
+		renderAllTasks();		
+	}, [setCountAddTask])
+
+	const renderAllTasks = ()=>{
 		axios.get(baseUrl).then((response)=>{
 			setTasks(response.data)
 			alert('buscou')
@@ -31,50 +35,58 @@ function App() {
 		}).catch(()=>{
 			alert('não buscou as tasks')
 		})
-		
-	}, [countAddTask])
+	}
 	
  	const onClickCreateTask = ()=>{
-		setCountAddTask(countAddTask+1)
 		const body={
 			text: form.task,
 			day: form.day
 		 }
 		axios.post(baseUrl, body).then(()=>{
 			alert('task criada')
+			renderAllTasks();
 		}).catch(()=>{
 			alert('task não criada')
 		})
+		
 	}	 
 
 	const handleInputs=(event)=>{
 		const {name, value}= event.target
 		setForm({...form, [name]:value});
-		console.log(form.day)
+	}
+
+	const onClickDeleteTasks=(id)=>{		
+		axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-mello-anapaula/${id}`).then(()=>{
+			alert('deletou')
+			renderAllTasks();
+		}).catch(()=>{
+			alert('não deletou')
+		})
 	}
 
 	tasks.forEach((task)=>{
 		switch (task.day){			
 			case "monday":
-				ordenateList.monday.push(task.text)
+				ordenateList.monday.push({text: task.text, id: task.id})
 				break;
 			case "tuesday":
-				ordenateList.tuesday.push(task.text)
+				ordenateList.tuesday.push({text: task.text, id: task.id})
 				break;
 			case "quarta":
-				ordenateList.wednesday.push(task.text)
+				ordenateList.wednesday.push({text: task.text, id: task.id})
 				break;
 			case "thursday":
-				ordenateList.thursday.push(task.text)
+				ordenateList.thursday.push({text: task.text, id: task.id})
 				break;
 			case "friday":
-				ordenateList.friday.push(task.text)
+				ordenateList.friday.push({text: task.text, id: task.id})
 				break;
 			case "saturday":
-				ordenateList.saturday.push(task.text)
+				ordenateList.saturday.push({text: task.text, id: task.id})
 				break;
 			case "sunday":
-				ordenateList.sunday.push(task.text)
+				ordenateList.sunday.push({text: task.text, id: task.id})
 				break;
 		}
 	 })
@@ -106,37 +118,37 @@ function App() {
   	  	    <div>
 				Segunda
 				{ordenateList.monday.map((task)=>{
-					return <p>{task}</p>})}
+					return <p>{task.text} <button onClick={() => {onClickDeleteTasks(task.id)}}>X</button></p>})}
 			</div>
   	  	    <div>
 				Terça
 				{ordenateList.tuesday.map((task)=>{
-					return <p>{task}</p>})}
+					return <p>{task.text} <button onClick={() => {onClickDeleteTasks(task.id)}}>X</button></p>})}
 			</div>
   	  	    <div>
 				Quarta
 				{ordenateList.wednesday.map((task)=>{
-					return <p>{task}</p>})}
+					return <p>{task.text} <button onClick={() => {onClickDeleteTasks(task.id)}}>X</button></p>})}
 			</div>
   	  	    <div>
 				Quinta
 				{ordenateList.thursday.map((task)=>{
-					return <p>{task}</p>})}
+					return <p>{task.text} <button onClick={() => {onClickDeleteTasks(task.id)}}>X</button></p>})}
 			</div>
   	  	    <div>
 				Sexta
 				{ordenateList.friday.map((task)=>{
-					return <p>{task}</p>})}
+					return <p>{task.text} <button onClick={() => {onClickDeleteTasks(task.id)}}>X</button></p>})}
 			</div>
   	  	    <div>
 				Sábado
 				{ordenateList.saturday.map((task)=>{
-					return <p>{task}</p>})}
+					return <p>{task.text} <button onClick={() => {onClickDeleteTasks(task.id)}}>X</button></p>})}
 			</div>
   	  	    <div>
 				Domingo
 				{ordenateList.sunday.map((task)=>{
-					return <p>{task}</p>})}
+					return <p>{task.text} <button onClick={() => {onClickDeleteTasks(task.id)}}>X</button></p>})}
 			</div>
   	  	</section>
   	  </div>
--- semana11/planner-testes/src/App.test.js
+++ semana11/planner-testes/src/App.test.js
@@ -1,13 +1,47 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent, wait } from "@testing-library/react";
+import App from "./App";
+import axios from 'axios'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 test('verifica se as tarefas estão sendo encontradas e renderizadas na tela', () =>{
+    axios.get=jest.fn().mockResolvedValue({
+      data: [
+        {
+          "day": 'terça' ,
+          "texto": "tarefa teste",
+          "id": "sYLgZCjyXTwr3G9lqNIP"
+        } 
+      ]
+	})
 	
+	const utils= render(<App/>)
+	const task=utils.getByText('tarefa teste')
+	expect(task).toBeInTheDocument()		
+	expect(axios.get).toHaveBeenCalled()
 })
+
+test('Verifica se uma nova tarefa é criada corretamente', async () => {
+	const baseUrl="https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-mello-anapaula"
+	axios.get = jest.fn().mockResolvedValue({
+		data: [{
+			day: "segunda",
+			texto: "tarefa teste",
+		  }]
+	  })
+
+	axios.post = jest.fn().mockResolvedValue()
+	const { getByPlaceholderText, getByText }= render(<App/>)
+	const input=getByPlaceholderText('Digite uma tarefa')
+	await userEvent.type(input, 'tarefa teste')
+	expect(input).toHaveValue('tarefa teste')
+	const button = getByText(/ADICIONAR/i)
+	userEvent.click(button)	
+	expect(axios.post).toHaveBeenCalledWith(baseUrl, {
+	  day: 'segunda',
+	  texto: 'tarefa teste',
+    })
+	await wait(() => expect(axios.get).toHaveBeenCalledTimes(2))
+    await wait(() => expect(input).toHaveValue(''))
+})	
