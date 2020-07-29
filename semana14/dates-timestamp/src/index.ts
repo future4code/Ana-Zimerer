b076fffebca37bfa44e85ebe9d3b4116b88fa2be
Ana Zimerer <ana.zimerer@gmail.com>
2020-07-29 15:36:46 -0300

    exercicio 3

--- semana14/dates-timestamp/src/index.ts
+++ semana14/dates-timestamp/src/index.ts
@@ -12,37 +12,34 @@ const AllEvents: event[]=[
     {
         name: 'Campo de geomorfologia',
         description: 'Excursão pra Serra da Canastra',
-        initialDate: moment("19 de Outubro de 2012 15:00", 'DD [de] MMM [de] YYYY HH:mm'),
-        finishDate: moment("22 de Outubro de 2012 18:00", 'DD [de] MMM [de] YYYY HH:mm')   
+        initialDate: moment("19 de Outubro de 2022 15:00", 'DD [de] MMM [de] YYYY HH:mm'),
+        finishDate: moment("22 de Outubro de 2022 18:00", 'DD [de] MMM [de] YYYY HH:mm')   
     },
     {
         name: 'Aula de Biomolecular',
         description: 'Laboratório - Sala 12',
-        initialDate: moment("17 de Outubro de 2012 10:00", 'DD [de] MMM [de] YYYY HH:mm'),
-        finishDate: moment("17 de Outubro de 2012 13:00", 'DD [de] MMM [de] YYYY HH:mm')   
+        initialDate: moment("17 de Outubro de 2020 10:00", 'DD [de] MMM [de] YYYY HH:mm'),
+        finishDate: moment("17 de Outubro de 2020 13:00", 'DD [de] MMM [de] YYYY HH:mm')   
     },
 ]
 
 function showsEvents(events: event[]): void{
     events.forEach((item:event)=>{
+        let duration = item.finishDate.diff(item.initialDate, "hours")
+        const today: moment.Moment = moment();
+        let daysToStart= item.initialDate.diff(today, "days" )
         console.log(`
             Nome: ${item.name}
             Descrição: ${item.description}
             Início: ${item.initialDate.format("dddd - DD [de] MMMM [de] YYYY, [ás] HH:mm")}
-            Fim: ${item.finishDate.format("dddd - DD [de] MMMM [de] YYYY, [ás] HH:mm")}						
+            Fim: ${item.finishDate.format("dddd - DD [de] MMMM [de] YYYY, [ás] HH:mm")}	
+            Duração: ${duration} horas.	
+            Começa em: ${daysToStart} dias			
 		`);
     })
 }
 
 showsEvents(AllEvents)
+//b) moment.locale('eng-gb')
 
-//Crie uma função que recebe o array de eventos criado no exercício anterior, e imprime algumas informações sobre cada um no console
-//
-//a) Exiba as informações do evento como uma string com o seguinte formato:
-//
-//```
-//Nome: Festa de aniversário
-//Horário de início: Segunda-feira, 25 de Junho de 2020, 12:00
-//Horário de fim: 25 de Junho de 2020, 18:30
-//Descrição: Festa super animada! Sua presença já é um presente.
-//```
+
