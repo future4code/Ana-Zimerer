187876afa0f405ea573d6f43c0dc68b19fe8a14e
Ana Zimerer <ana.zimerer@gmail.com>
2020-08-12 22:04:28 -0300

    Exercício 1

--- semana16/knex-express/Exercícios.md
+++ semana16/knex-express/Exercícios.md
@@ -1,3 +1,8 @@
 1. cria um conection, com os dados todos no pacote env. 
 2. Após isso, já podemos criar um knex raw, que fará a ligação do código com o banco 
     a. precisa ser assincrona, com retorno promisse<void>, retorno de fora 
+
+<h3> EXERCÍCIO 1 </h3>
+<p> a) Para novas consultas, o ideal é pegar uma resposta que venha somente a resposta da query. Quando pegamos uma resposta de uma query a mesma vem com diversas informações de sua criação além da reposta que queremos. Por isso, usamos pegamos apenas a posição 0 do array, para que a resposta não fique poluída.</p>
+<p>b) Resposta no arquivo index.ts<p>
+<p>c) Resposta no arquivo index.ts<p>
--- semana16/knex-express/src/index.ts
+++ semana16/knex-express/src/index.ts
@@ -1,10 +1,10 @@
 import knex from "knex";
 import dotenv from "dotenv";
-import express from "express";
-
-import { AddressInfo } from "net";
-import { promises } from "fs";
-
+//import express from "express";
+//
+//import { AddressInfo } from "net";
+//import { promises } from "fs";
+//
 dotenv.config();
 
 const connection = knex({
@@ -18,43 +18,20 @@ const connection = knex({
   },
 });
 
-async function createTable(): Promise<void>{
-    try{
-        await connection.raw(`
-        CREATE TABLE Pokemon
-        (
-            id INT PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            type VARCHAR(255) NOT NULL
-        );
-        `);
-        console.log('Sucesso')
-    }catch{
-        console.log('Error')
-    }
+const getActorByName= async(name: string): Promise<void>=>{
+    const result= await connection.raw(`
+    SELECT * FROM Actor WHERE name = '${name}'
+    `)
+    console.table(result[0])
 }
 
-const getActorById = async (id: string): Promise<any> => {
-    const result = await connection.raw(`
-      SELECT * FROM Actor WHERE id = '${id}'
+//getActorByName("Faustão")
+
+const QuantityOfActorsByGender= async(gender:string): Promise<any>=>{
+    const result= await connection.raw(`
+        SELECT COUNT (*) FROM Actor WHERE gender= '${gender}'
     `)
-  
-      return result[0][0]
-  }
+    console.table(result[0])
+}
 
-getActorById("007")
-
-
-
-//const app = express();
-//
-//app.use(express.json());
-//
-//const server = app.listen(process.env.PORT || 3003, () => {
-//  if (server) {
-//    const address = server.address() as AddressInfo;
-//    console.log(`Server is running in http://localhost:${address.port}`);
-//  } else {
-//    console.error(`Failure upon starting server.`);
-//  }
-//});
+QuantityOfActorsByGender('male')
