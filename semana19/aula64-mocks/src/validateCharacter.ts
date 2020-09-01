4cbc6da20373307b65c907ac932e8a9a7217a85e
Ana Zimerer <ana.zimerer@gmail.com>
2020-09-01 16:35:28 -0300

    Exercício 2

--- semana19/aula64-mocks/src/validateCharacter.ts
+++ semana19/aula64-mocks/src/validateCharacter.ts
@@ -9,18 +9,30 @@ export function validateCharacter(character: Character) {
     try {
         if (!character.name || !character.life || !character.strength || !character.defense) {
             throw new Error('Fill all the blanks')
+            console.log("valores invalidos")
         }
 
         if (character.life <= 0) {
+            console.log("vida menor que zero")
             throw new Error('You lost the game. Your life must to be greater than 0' + character.life)
         }
 
-        if (character.strength || character.defense < 0) {
+        if (character.strength < 0 || character.defense < 0) {
+            console.log("forças menores que zero ")
             throw new Error('You cant start the game with strength and defense less than 0')
         }
+
         return true
     } catch{
         return false
     }
 }
 
+const newCharacter: Character = {
+    name: 'Lux',
+    life: 50,
+    strength: 50,
+    defense: 60
+}
+
+console.log(validateCharacter(newCharacter))
